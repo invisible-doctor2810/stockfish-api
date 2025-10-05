ae2eabaea6cea6833f9c79114b7415d13c6fe179
Mudit Bhatnagar <114670058+invisible-doctor2810@users.noreply.github.com>
2025-10-05 16:12:05 -0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -5,12 +5,17 @@ echo "Installing dependencies..."
 pip install --upgrade pip
 
 echo "Downloading Stockfish..."
-curl -L -o stockfish.zip https://github.com/official-stockfish/Stockfish/releases/download/sf_17/stockfish-ubuntu-x86-64-avx2.zip
+# Official GitHub asset direct link (works with curl on Render)
+curl -L -o stockfish.zip \
+  https://github.com/official-stockfish/Stockfish/releases/download/sf_17/stockfish-ubuntu-x86-64-avx2.zip
+
+echo "Verifying file size..."
+ls -lh stockfish.zip
 
 echo "Unzipping Stockfish..."
 unzip stockfish.zip -d stockfish
-chmod +x stockfish/stockfish-ubuntu-x86-64-avx2
-mv stockfish/stockfish-ubuntu-x86-64-avx2 stockfish_engine
+chmod +x stockfish/stockfish*
+mv stockfish/stockfish* stockfish_engine
 
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
