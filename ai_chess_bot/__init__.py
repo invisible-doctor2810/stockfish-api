c1747b51177c91e16acd1a08a9382225285c08a2
Mudit Bhatnagar <114670058+invisible-doctor2810@users.noreply.github.com>
2025-10-05 14:42:35 -0500

    Delete ai_chess_bot directory

--- ai_chess_bot/__init__.py
+++
@@ -1,7 +0,0 @@
-# ai_chess_bot/__init__.py
-"""
-ai_chess_bot package
-
-Keep this file minimal to avoid executing package modules at import time.
-This is the safest default while you develop the package.
-"""
